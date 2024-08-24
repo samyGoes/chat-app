baffdcb3a8b72646e637560fe26e1965b19e8cf3
Sâmily Goes <samily.goes@etec.sp.gov.br>
2024-08-24 09:54:24 -0300

    Deslizar do App ajustado

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+//#region VARIÁVEIS //
 const menu = document.querySelector(".tela-celular-cabecalho-opcao-menu");
 const setaVoltar = document.querySelector("#tela-celular-cabecalho-box-seta");
 const setaInicial = document.querySelector("#tela-celular-inicial-seta");
-// MUDANDO TEMA
+// MUDANDO TEMA //
 const telaCelular = document.querySelector(".tela-celular");
 const boxMinhasMsg = document.querySelector(".tela-celular-box-mensagens-voce")
 const boxOutroMsg = document.querySelector(".tela-celular-box-mensagens-outro");
 const foto = document.querySelector(".box-img-voce");
+//#endregion
 
+
+//#region FUNÇÕES //
 function menuLateral()
 {
     if (menu.style.display == "none" || menu.style.display == "")
@@ -35,8 +39,6 @@ function mudaTema()
 
         boxOutroMsg.style.backgroundColor = "hsl(238.15 28.53% 38.71%)";
         boxOutroMsg.style.color = "hsl(271 68.54% 87.96%)";
-   
-    
 }
 //}
 document.querySelector(".tela-celular-cabecalho-opcao-menu").addEventListener("click", mudaTema);
@@ -52,3 +54,4 @@ function abrirApp()
     telaCelular.style.transform = "translateX(0px)";
 }
 setaInicial.addEventListener("click", abrirApp);
+//#endregion
