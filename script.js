ad58f6325574daa10111348363b8938de0a41ce5
Sâmily Goes <samily.goes@etec.sp.gov.br>
2024-08-24 12:24:53 -0300

    #1 atualização da opção de mudar tema

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 //#region VARIÁVEIS //
-const menu = document.querySelector(".tela-celular-cabecalho-opcao-menu");
 const setaVoltar = document.querySelector("#tela-celular-cabecalho-box-seta");
 const setaInicial = document.querySelector("#tela-celular-inicial-seta");
-// MUDANDO TEMA //
-const telaCelular = document.querySelector(".tela-celular");
-const boxMinhasMsg = document.querySelector(".tela-celular-box-mensagens-voce")
-const boxOutroMsg = document.querySelector(".tela-celular-box-mensagens-outro");
-const foto = document.querySelector(".box-img-voce");
+    //#region MENU LATERAL //
+    const telaCelular = document.querySelector(".tela-celular");
+    let tema = "";
+    // MENU //
+    const btnMenu = document.querySelector("#tela-celular-cabecalho-btn-menu");
+    const menu = document.querySelector(".tela-celular-cabecalho-menu");
+    const btnMudarTemaMenu = document.querySelector(".tela-celular-cabecalho-menu-btn-tema")
+    // MENSAGENS //
+    const boxMinhasMsg = document.querySelectorAll(".tela-celular-box-mensagens-voce")
+    const boxOutroMsg = document.querySelectorAll(".tela-celular-box-mensagens-outro");
+
+    const cabecalho = document.querySelector(".tela-celular-cabecalho");
+    //#endregion
 //#endregion
 
 
 //#region FUNÇÕES //
+
 function menuLateral()
 {
     if (menu.style.display == "none" || menu.style.display == "")
@@ -22,26 +30,56 @@ function menuLateral()
         menu.style.display = "none";
     }
 }
-document.querySelector("#tela-celular-cabecalho-menu").addEventListener("click", menuLateral);
+btnMenu.addEventListener("click", menuLateral);
 
-
-// for(let i = 1; i <= boxMinhasMsg.length; i++)
-    // {
 function mudaTema() 
-{
-    
-        telaCelular.style.backgroundColor = "hsl(282 27.82% 20.44%)";
+{   
+    btnMudarTemaMenu.addEventListener("click", function()
+    {
+        //#region DEIXANDO O TEMA ESCURO //
+        if(tema == "" || tema == "claro") 
+        {
+            telaCelular.style.backgroundColor = "hsl(281.38 47.54% 11.96%)";
+            cabecalho.style.background = "hsl(241.4 27.82% 20.44%)";
+            cabecalho.style.border = "1px solid hsl(254.77 28.89% 36.16%)";
 
-        boxMinhasMsg.style.backgroundColor = "hsl(293.45 42.33% 29.98%)";
-        // boxMinhasMsg.style.border = "1px solid hsl(271 11.61% 53.84%)";
-        boxMinhasMsg.style.color = "hsl(271 68.54% 87.96%)";
-        foto.style.border = "1px solid hsl(271 11.61% 53.84%)";
+            for(let i = 0; i < boxMinhasMsg.length; i++)
+            {     
+                boxMinhasMsg[i].style.backgroundColor = "hsl(293.45 42.33% 29.98%)";
+                boxMinhasMsg[i].style.color = "hsl(271 68.54% 87.96%)"; 
+            }
+            for(let i = 0; i < boxOutroMsg.length; i++)
+            {
+                boxOutroMsg[i].style.backgroundColor = "hsl(238.15 28.53% 38.71%)";
+                boxOutroMsg[i].style.color = "hsl(271 68.54% 87.96%)"; 
+            }  
+            tema = "escuro";
+        }
+        //#endregion
+        
+        //#region DEIXANDO O TEMA CLARO //
+        else 
+        {
+            telaCelular.style.backgroundColor = "hsl(270, 20%, 96%)";
+            cabecalho.style.background = "linear-gradient(to right, hsl(264, 100%, 61%), hsl(293, 100%, 63%))";
+            cabecalho.style.border = "1px solid hsl(270, 20%, 96%)";
 
-        boxOutroMsg.style.backgroundColor = "hsl(238.15 28.53% 38.71%)";
-        boxOutroMsg.style.color = "hsl(271 68.54% 87.96%)";
+            for(let i = 0; i < boxMinhasMsg.length; i++)
+            {     
+                boxMinhasMsg[i].style.backgroundColor = "hsl(0, 0%, 100%)";
+                boxMinhasMsg[i].style.color = "hsl(271, 15%, 43%)"; 
+            }
+            for(let i = 0; i < boxOutroMsg.length; i++)
+            {
+                boxOutroMsg[i].style.backgroundColor = "hsl(272, 57%, 91%)";
+                boxOutroMsg[i].style.color = "hsl(276, 55%, 52%)"; 
+            }  
+            tema = "claro";
+        }
+        //#endregion
+    });        
 }
-//}
-document.querySelector(".tela-celular-cabecalho-opcao-menu").addEventListener("click", mudaTema);
+mudaTema();
 
 function voltar()
 {
@@ -54,4 +92,5 @@ function abrirApp()
     telaCelular.style.transform = "translateX(0px)";
 }
 setaInicial.addEventListener("click", abrirApp);
+
 //#endregion
